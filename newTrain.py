99af232af872089b5d8f77e859ec6284e564dbf2
Andrea <bbooss97@gmail.com>
2023-04-27 12:38:39 +0200

    more augmentation

--- newTrain.py
+++ newTrain.py
@@ -35,7 +35,7 @@ if sweep_id=="":
 def loadDatasetCifar100():
     #define dataset
     datasetTraining=torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transforms.Compose([
-        transforms.RandAugment(num_ops=3, magnitude=5),
+        transforms.RandAugment(num_ops=3, magnitude=10),
         transforms.ToTensor(),
         transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
     ]))
@@ -55,7 +55,7 @@ def loadDatasetCifar100():
 def loadDatasetCifar10():
     #define dataset
     datasetTraining=torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.Compose([
-        transforms.RandAugment(num_ops=3, magnitude=5),
+        transforms.RandAugment(num_ops=3, magnitude=10),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]))
